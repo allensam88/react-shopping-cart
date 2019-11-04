794462f6097c57398ce36b59ac4a56eea84e0915
allensam88 <54912919+allensam88@users.noreply.github.com>
2019-11-04 13:20:36 -0800

    got up to Step 4 done, created context for products

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import { ProductContext } from './context/ProductContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -13,28 +14,29 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item]);
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
-
+			
+				<Navigation cart={cart} />
+			
+			
 			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+			<ProductContext.Provider value={{ products, addItem }}>
+				<Route
+					exact path="/" 
+					component={Products}
+				/>
+			</ProductContext.Provider>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+			
+				<Route
+					path="/cart"
+					render={() => <ShoppingCart cart={cart} />}
+				/>
+			
 		</div>
 	);
 }
